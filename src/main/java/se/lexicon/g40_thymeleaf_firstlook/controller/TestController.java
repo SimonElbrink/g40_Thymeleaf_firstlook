b66b59bd69d342ca5cc8f3ed0e5a65fa5eb9230b
Simon Elbrink <simon.elbrink@gmail.com>
2022-03-14 12:26:59 +0100

    Test Controller Variable and Formatting

--- src/main/java/se/lexicon/g40_thymeleaf_firstlook/controller/TestController.java
+++ src/main/java/se/lexicon/g40_thymeleaf_firstlook/controller/TestController.java
@@ -1,10 +1,13 @@
-package se.lexicon.g40_thymeleaf_firstlook;
+package se.lexicon.g40_thymeleaf_firstlook.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.sql.Date;
+import java.time.LocalDate;
 
 @Controller
 public class TestController {
@@ -18,7 +21,8 @@ public class TestController {
     public String greeting(Model model){
         String message = "Hello Group 40";
 
-        model.addAttribute("text", message);
+        model.addAttribute("message", message);
+        model.addAttribute("serverTime", Date.valueOf(LocalDate.now()));
         return "greeting";
     }
 
