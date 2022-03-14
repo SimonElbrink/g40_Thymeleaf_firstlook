b7f2ab59e9f3e7c2076d08fae6845d2aefa76b53
Simon Elbrink <simon.elbrink@gmail.com>
2022-03-14 10:06:45 +0100

    TestController adding Model to send data to webpage.

--- src/main/java/se/lexicon/g40_thymeleaf_firstlook/TestController.java
+++ src/main/java/se/lexicon/g40_thymeleaf_firstlook/TestController.java
@@ -1,6 +1,8 @@
 package se.lexicon.g40_thymeleaf_firstlook;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,5 +13,14 @@ public class TestController {
     public String helloWorld(){
         return "hello";
     }
+
+    @GetMapping("/greeting")
+    public String greeting(Model model){
+        String message = "Hello Group 40";
+
+        model.addAttribute("text", message);
+        return "greeting";
+    }
+
 
 }
