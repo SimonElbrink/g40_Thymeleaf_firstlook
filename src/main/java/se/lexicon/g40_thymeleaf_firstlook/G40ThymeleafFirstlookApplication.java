e58afcfcdd0179e112a41042c39e8a3d6bec9603
Simon Elbrink <simon.elbrink@gmail.com>
2022-03-14 10:02:10 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/g40_thymeleaf_firstlook/G40ThymeleafFirstlookApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g40_thymeleaf_firstlook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G40ThymeleafFirstlookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G40ThymeleafFirstlookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g40_thymeleaf_firstlook/G40ThymeleafFirstlookApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g40_thymeleaf_firstlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G40ThymeleafFirstlookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
